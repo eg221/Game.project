4e88d116b53676120f9b479ee8ec986356efc256
eg221 <knysenkoegor@gmail.com>
2025-05-28 22:29:21 +0300

    Sec commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField, Min(0f)]
+    float
+        maxXSpeed = 20f,
+        startXSpeed = 8f,
+        constantYSpeed = 10f,
+        extents = 0.5f;
+
+    Vector2 position, velocity;
+    public Vector2 Position => position;
+    public float Extents => extents;
+    public Vector2 Velocity => velocity;
+
+    void Awake() => gameObject.SetActive(false);
+    public void UpdateVisualization() =>
+        transform.localPosition = new Vector3(position.x, 0f, position.y);
+
+    public void Move() => position += velocity * Time.deltaTime;
+
+    public void StartNewGame()
+    {
+        position = Vector2.zero;
+        UpdateVisualization();
+        velocity = new Vector2(startXSpeed, -constantYSpeed);
+        gameObject.SetActive(true);
+    }
+    public void EndGame()
+    {
+        position.x = 0f;
+        gameObject.SetActive(false);
+    }
+
+    public void SetXPositionAndSpeed(float start, float speedFactor, float deltaTime)
+    {
+        velocity.x = maxXSpeed * speedFactor;
+        position.x = start + velocity.x * deltaTime;
+    }
+
+    public void BounceX(float boundary)
+    {
+        position.x = 2f * boundary - position.x;
+        velocity.x = -velocity.x;
+    }
+
+    public void BounceY(float boundary)
+    {
+        position.y = 2f * boundary - position.y;
+        velocity.y = -velocity.y;
+    }
+}
